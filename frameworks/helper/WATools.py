5255fa14441b084129d451311263f301b254e13f
junaid-ahmed-init <junaid.ahmed@init-global.com>
2025-09-09 13:16:05 +0500

    Update WATools.py

--- frameworks/helper/WATools.py
+++ frameworks/helper/WATools.py
@@ -9,7 +9,7 @@ import time
 ## --others '{"WA": {"region": "ap-southeast-1", "reportName":"SS_Report", "newMileStone":0}}'
 
 class WATools():
-    DEFAULT_REPORTNAME = 'Buy2day - Web App'
+    DEFAULT_REPORTNAME = 'Buy2day - admin App'
     DEFAULT_NEWMILESTONE = 0
     waInfo = {
         'isExists': False,
