659df2694a317e8b15f08618c68a64c01f005a90
junaid-ahmed-init <junaid.ahmed@init-global.com>
2026-07-08 15:58:25 +0500

    Update WATools.py

--- frameworks/helper/WATools.py
+++ frameworks/helper/WATools.py
@@ -9,7 +9,7 @@ import time
 ## --others '{"WA": {"region": "ap-southeast-1", "reportName":"SS_Report", "newMileStone":0}}'
 
 class WATools():
-    DEFAULT_REPORTNAME = 'Befiler - Chatbot'
+    DEFAULT_REPORTNAME = 'Ojin - Backend Revamp'
     DEFAULT_NEWMILESTONE = 0
     waInfo = {
         'isExists': False,
