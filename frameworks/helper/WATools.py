46f4c61e34e82726d50fb24153699ce562c25cff
junaid-ahmed-init <junaid.ahmed@init-global.com>
2026-07-16 13:51:22 +0500

    Update WATools.py

--- frameworks/helper/WATools.py
+++ frameworks/helper/WATools.py
@@ -9,7 +9,7 @@ import time
 ## --others '{"WA": {"region": "ap-southeast-1", "reportName":"SS_Report", "newMileStone":0}}'
 
 class WATools():
-    DEFAULT_REPORTNAME = 'Abhi - Core Workload'
+    DEFAULT_REPORTNAME = 'Abhi - Mobile App'
     DEFAULT_NEWMILESTONE = 0
     waInfo = {
         'isExists': False,
